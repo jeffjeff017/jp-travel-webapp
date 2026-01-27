44d7772b97a856a3c48bcb529f92e997574e69f7
Jeff Chan <fonglongchan1018@gmail.com>
2026-01-27 10:53:08 +0800

    Fix setAddingToTrip typo to setShowAddToTrip

--- src/components/WishlistButton.tsx
+++ src/components/WishlistButton.tsx
@@ -897,7 +897,7 @@ export default function WishlistButton({
                                       </div>
                                       <div className="flex gap-2 mt-2">
                                         <button
-                                          onClick={() => setAddingToTrip(null)}
+                                          onClick={() => setShowAddToTrip(null)}
                                           className="flex-1 py-2 bg-gray-200 hover:bg-gray-300 text-gray-700 rounded-lg text-sm transition-colors"
                                         >
                                           取消
