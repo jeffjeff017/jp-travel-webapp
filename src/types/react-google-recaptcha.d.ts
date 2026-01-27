7d0e5a73752474b709760e85323a2572c001ada3
Jeff Chan <fonglongchan1018@gmail.com>
2026-01-27 10:49:34 +0800

    Add type declaration for react-google-recaptcha

---
+++ src/types/react-google-recaptcha.d.ts
@@ -0,0 +1,22 @@
+declare module 'react-google-recaptcha' {
+  import * as React from 'react'
+
+  export interface ReCAPTCHAProps {
+    sitekey: string
+    onChange?: (token: string | null) => void
+    onExpired?: () => void
+    onErrored?: () => void
+    theme?: 'light' | 'dark'
+    size?: 'compact' | 'normal' | 'invisible'
+    tabindex?: number
+    hl?: string
+    badge?: 'bottomright' | 'bottomleft' | 'inline'
+  }
+
+  export default class ReCAPTCHA extends React.Component<ReCAPTCHAProps> {
+    reset(): void
+    execute(): void
+    executeAsync(): Promise<string>
+    getValue(): string | null
+  }
+}
